16668528cb28cb431c3ba347ed9bf2b99f02166c
juhyunlee <ljh01099014032@gmail.com>
2025-03-26 13:59:34 +0900

    로그인 유무에 따른 nav구조 변경

--- accounts/views.py
+++ accounts/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render, redirect
 from .forms import CustomUserCreationForm, CustomAuthenticationForm
 from django.contrib.auth import login as auth_login
+from django.contrib.auth import logout as auth_logout
 # Create your views here.
 def signup(request):
     if request.method == "POST":
@@ -22,7 +23,7 @@ def login(request):
         form = CustomAuthenticationForm(request, request.POST)
         if form.is_valid():
             auth_login(request, form.get_user())
-            return redirect('articles:index')
+            return redirect('accounts:login')
 
     else:
         form = CustomAuthenticationForm()
@@ -32,3 +33,7 @@ def login(request):
     }
 
     return render(request, 'login.html', context)
+
+def logout(request):
+    auth_logout(request)
+    return redirect('accounts:login')
